3cce112ed5b31932117931a7434a314df9352375
Naveya Jintaya <naveya@divertise.asis>
2024-02-02 15:44:10 +0700

    Sending reset password email when a user request to reset password.

---
+++ simple_renting_book_server/user_manager/signals.py
@@ -0,0 +1,48 @@
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.dispatch import receiver
+from django.template.loader import render_to_string
+from django_rest_passwordreset.signals import reset_password_token_created
+
+FRONT_END_URL = getattr(settings, "FRONT_END_URL", "")
+DEFAULT_FROM_EMAIL = getattr(settings, "DEFAULT_FROM_EMAIL", "")
+WEBSITE_TITLE = getattr(settings, "WEBSITE_TITLE", "")
+
+
+@receiver(reset_password_token_created)
+def password_reset_token_created(
+    sender, instance, reset_password_token, *args, **kwargs
+):
+    """
+    Handles password reset tokens
+    When a token is created, an e-mail needs to be sent to the user
+    :param sender: View Class that sent the signal
+    :param instance: View Instance that sent the signal
+    :param reset_password_token: Token Model Object
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    context = {
+        "current_user": reset_password_token.user,
+        "email": reset_password_token.user.email,
+        "reset_password_url": "{}?token={}".format(
+            FRONT_END_URL, reset_password_token.key
+        ),
+    }
+
+    # render email text
+    email_html_message = render_to_string("./email/user_reset_password.html", context)
+    email_plaintext_message = render_to_string(
+        "./email/user_reset_password.txt", context
+    )
+
+    msg = EmailMultiAlternatives(
+        subject=f"Password Reset for {WEBSITE_TITLE}",
+        body=email_plaintext_message,
+        from_email=DEFAULT_FROM_EMAIL,
+        to=[reset_password_token.user.email],
+    )
+    msg.attach_alternative(email_html_message, "text/html")
+    msg.send()
+    # TODO: Setup MailCatcher
