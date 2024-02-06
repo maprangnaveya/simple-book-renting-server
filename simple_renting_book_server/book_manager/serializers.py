23d09f034fa0621f43eac5c355487a28bc184c7e
Naveya Jintaya <naveya@divertise.asis>
2024-02-07 00:44:36 +0700

    Add mockup `available_in_store` in book serializer.

--- simple_renting_book_server/book_manager/serializers.py
+++ simple_renting_book_server/book_manager/serializers.py
@@ -6,6 +6,11 @@ from book_manager.models import Book
 class BookSerializer(serializers.ModelSerializer):
     authors = serializers.SlugRelatedField(many=True, read_only=True, slug_field="name")
     image = serializers.ImageField(read_only=True)
+    available_in_store = serializers.SerializerMethodField()
+
+    def get_available_in_store(self, obj):
+        # TODO: Get an available in store
+        return obj.total_in_store
 
     class Meta:
         model = Book
@@ -16,5 +21,6 @@ class BookSerializer(serializers.ModelSerializer):
             "authors",
             "published_at",
             "total_in_store",
+            "available_in_store",
             "image",
         )
